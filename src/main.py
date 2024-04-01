bf44f9ec7e8154c17c679bf51c78b516400e0bfb
LawrenceJavier <74473715+LawrenceJavier@users.noreply.github.com>
2024-04-01 21:30:53 +0200

    NUEVO MODELO

--- src/main.py
+++ src/main.py
@@ -39,8 +39,14 @@ def get_label(imagen):
 
 
 
+    try:
+        #Test GPU
+        model_weights = torch.load('./src/resnet50-final.pt')
+    except:
+        #En caso de que no haya GPU, se carga a la CPU
+        print("Using CPU...")
+        model_weights = torch.load('./src/resnet50-final.pt', map_location=torch.device('cpu'))
 
-    model_weights = torch.load('./src/modelo.pth')
     my_trained_model = CNN(torchvision.models.resnet50(weights='DEFAULT'), num_classes)
     my_trained_model.load_state_dict(model_weights)
 
